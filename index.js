f1e3c19274390a2821bb9a548db2ed89007ac4a5
autumn-ragland <raglanda998@gmail.com>
2019-08-13 13:55:38 -0500

    Graded 08-13-2019 13:54:35

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ allNames[1]=" jasmine";
 allNames[2]=" harold";
 allNames[3]=" eddie";
     alert([allNames]);
+
 let numberArr=[13,23,24,1,11];
 alert([numberArr]);
+// PARTIAL CREDIT : use an array to print each element in the array
 
 let theNames=["Martin","Gina", "Pam","Tommy"];
 alert(theNames[2]);
